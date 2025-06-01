3d978ceff97119ef329469a62e959d45b91e1dfe
Aron Soto <115325057+AronSoto@users.noreply.github.com>
2025-06-01 13:44:15 -0500

    Refactor(assets): Standardize quotes and formatting in JavaScript and CSS files.
    Co-authored-by: Aron Soto <aronchino2g@hotmail.com>

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const heroSection = document.getElementById('hero-section');
-    const images = document.querySelectorAll('.carousel .image');
-    let current = 0;
+document.addEventListener("DOMContentLoaded", function () {
+  const heroSection = document.getElementById("hero-section");
+  const images = document.querySelectorAll(".carousel .image");
+  let current = 0;
 
-    function showNextImage() {
-        images[current].classList.remove('active');
-        current = (current + 1) % images.length;
-        images[current].classList.add('active');
-    }
+  function showNextImage() {
+    images[current].classList.remove("active");
+    current = (current + 1) % images.length;
+    images[current].classList.add("active");
+  }
 
-    images[current].classList.add('active');
+  images[current].classList.add("active");
 
-    setTimeout(function() {
-        heroSection.style.backgroundImage = 'none';
-        setInterval(showNextImage, 9000);
-    }, 1000);
+  setTimeout(function () {
+    heroSection.style.backgroundImage = "none";
+    setInterval(showNextImage, 9000);
+  }, 1000);
 });
-
